61f22aa33857856539ddf8b3d49839ba8b4e76ef
sztoslol <kubuszeski05@wp.pl>
2022-11-21 15:20:01 +0100

    Prawoasd

--- app/src/main/java/com/mhb/zadanienaocene/MainActivity.kt
+++ app/src/main/java/com/mhb/zadanienaocene/MainActivity.kt
@@ -9,8 +9,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var i=0
-            findViewById<Button>(R.id.lewo).setOnClickListener {
+        var i=1
+            findViewById<Button>(R.id.prawo).setOnClickListener {
                 i+=1
                 if(i==1) {
                     findViewById<ImageView>(R.id.zdjecie).setImageResource(R.drawable.truskawka)
@@ -27,20 +27,18 @@ class MainActivity : AppCompatActivity() {
 
 
             }
-        var j=0
-        findViewById<Button>(R.id.prawo).setOnClickListener {
-            j+=1
-            if(j==1) {
+        findViewById<Button>(R.id.lewo).setOnClickListener {
+            i-=1
+            if(i==1) {
                 findViewById<ImageView>(R.id.zdjecie).setImageResource(R.drawable.truskawka)
             }
 
-            if(j==2) {
+            if(i==2) {
                 findViewById<ImageView>(R.id.zdjecie).setImageResource(R.drawable.czeresnie)
             }
 
-            if(j==3) {
+            if(i==3) {
                 findViewById<ImageView>(R.id.zdjecie).setImageResource(R.drawable.gruszki)
-                j=0
             }
 
 
