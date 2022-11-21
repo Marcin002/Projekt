18a6c56fa2c4c28bef5d7bfaba6f44b77fa070d3
sztoslol <kubuszeski05@wp.pl>
2022-11-21 13:56:14 +0100

    Initial commit

---
+++ app/src/main/java/com/mhb/zadanienaocene/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mhb.zadanienaocene
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
