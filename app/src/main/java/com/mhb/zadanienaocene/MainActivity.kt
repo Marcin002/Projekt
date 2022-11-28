04aecc44d8a29ba80338bf57a9c517659debfbd9
PC1 <marcinkummer99@gmail.com>
2022-11-28 19:03:47 +0100

    Zadanie

--- app/src/main/java/com/mhb/zadanienaocene/MainActivity.kt
+++ app/src/main/java/com/mhb/zadanienaocene/MainActivity.kt
@@ -3,12 +3,26 @@ package com.mhb.zadanienaocene
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
+import android.widget.TextView
+
+fun silnia(liczba: Int): Int{
+    if(liczba>1){
+        return liczba * silnia(liczba-1)
+    }
+    else{
+        return 0
+    }
+}
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         var i=1
             findViewById<Button>(R.id.prawo).setOnClickListener {
                 i+=1
@@ -29,6 +43,9 @@ class MainActivity : AppCompatActivity() {
             }
         findViewById<Button>(R.id.lewo).setOnClickListener {
             i-=1
+            if(i<=0){
+                i=3
+            }
             if(i==1) {
                 findViewById<ImageView>(R.id.zdjecie).setImageResource(R.drawable.truskawka)
             }
@@ -42,6 +59,12 @@ class MainActivity : AppCompatActivity() {
             }
 
 
+        }
+        findViewById<Button>(R.id.silnia).setOnClickListener {
+            val x=findViewById<EditText>(R.id.wprowadz)
+            val y=x.text.toString().toInt()
+            val wynik= silnia(y).toString()
+            findViewById<TextView>(R.id.wynik).text=wynik
         }
         }
     }
